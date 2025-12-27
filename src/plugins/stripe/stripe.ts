a0d21dd818ab7f7fdb0942456941dad50cebbb58
William <89201940+wylvo@users.noreply.github.com>
2025-12-26 20:16:08 -0500

    update package.json & stripe version

--- src/plugins/stripe/stripe.ts
+++ src/plugins/stripe/stripe.ts
@@ -10,7 +10,7 @@ declare module "fastify" {
 }
 
 const stripePlugin: FastifyPluginCallback = (fastify, _options, done) => {
-  const apiVersion = fastify.env.STRIPE_API_VERSION as "2025-09-30.clover"
+  const apiVersion = fastify.env.STRIPE_API_VERSION as "2025-12-15.clover"
 
   if (fastify.env.NODE_ENV === "development") {
     const testSecretKey = fastify.env.STRIPE_TEST_SECRET_KEY
